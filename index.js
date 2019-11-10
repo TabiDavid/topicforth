b67f8edd919565e4f56dfe483a07f7cb44ea1107
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-09 19:25:37 -0500

    commit

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ function bringdata(){
 
     const xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "https://randomuser.me/api", true);
+    xhttp.open("GET", "https://mindicador.cl/api/dolar", true);
 
     xhttp.send();
 
     xhttp.onreadystatechange = function(){
 if (this.readyState == 4 && this.status == 200){
+
     document.querySelector("#answer").innerHTML = this.responseText;
 
+
+    //document.getElementById("outPut").innerHTML
+//var result = JSON.parse(this.responseText);
+//document.getElementById("#answer").innerHTML = result.name + result.username;
 }
 
     }
