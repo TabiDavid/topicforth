7e3e7f0da952d4e4859d68a7d63627af90d9d97c
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-20 21:53:17 -0500

    commit

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-//console.log('active');
-
-document.querySelector("#button").addEventListener("click", bringdata);
-
-function bringdata(){
-
-    //console.log("function active");
-
-    const xhttp = new XMLHttpRequest();
-
-    xhttp.open("GET", "https://mindicador.cl/api/dolar", true);
-
-    xhttp.send();
-
-    xhttp.onreadystatechange = function(){
-if (this.readyState == 4 && this.status == 200){
-
-    document.querySelector("#answer").innerHTML = this.responseText;
-
-}
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var result = JSON.parse(this.responseText);
+        
+        document.getElementById("demo").innerHTML = " Currency: " + result.euro.nombre + "<br>" + "Compare value with: " + result.euro.unidad_medida + "<br>" + " Date: " + result.euro.fecha + 
+        "<br>" + "Value: " + result.euro.valor;
+  
+        document.getElementById("demo2").innerHTML = " Currency: " + result.dolar.nombre + "<br>" + "Compare value with: " + result.dolar.unidad_medida + "<br>" + " Date: " + result.dolar.fecha + 
+        "<br>" + "Value: " + result.dolar.valor;
+     
+        document.getElementById("demo3").innerHTML = " Currency: " + result.bitcoin.nombre + "<br>" + "Compare value with: " + result.bitcoin.unidad_medida + "<br>" + " Date: " + result.bitcoin.fecha + 
+        "<br>" + "Value: " + result.bitcoin.valor;
 
     }
+};
+xhttp.open("GET", "https://mindicador.cl/api", true);
+xhttp.send();
 }
